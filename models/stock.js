371dc352bcbaaae4cb811aa7e81cb3a2c794c92d
mchxyz <michellearguelles@protonmail.com>
2023-10-26 21:43:45 -0400

    remove env file

--- models/stock.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const stockSchema = new Schema({
-  ticker: String,
-  lastUpdated: Date,
-  price: Number,
-});
-
-module.exports = model("stock", stockSchema);
